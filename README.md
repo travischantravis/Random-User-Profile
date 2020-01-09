f2ac0378ef6777f5b0f9932dabb9d0cc106651bb
travischantravis <chancheukyin4463@gmail.com>
2020-01-09 13:02:29 -0800

    Added api call

--- README.md
+++ README.md
@@ -3,4 +3,19 @@
 ### By Travis
 
 > beginning my coding career in 2020
-> Images from random user generator
+
+Some useful CSS I used
+
+```
+display: flex;
+justify-content: center;
+align-items: center;
+```
+
+Some useful JS I used
+
+```
+fetch(url).then().catch()
+```
+
+Images are from [Random User generator](https://randomuser.me/photos) API
--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+const url = "https://randomuser.me/api/?results=3";
 const btn = document.querySelectorAll("button");
+const name = document.querySelectorAll(".name");
+const username = document.querySelectorAll(".username");
+const img = document.querySelectorAll("img");
 
-const name = document.querySelectorAll("h4");
+// Generate random user information from API fetch
+fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data.results);
+    const users = data.results;
+    users.forEach((user, index) => {
+      img[index].src = user.picture.thumbnail;
+      name[index].innerHTML = `${user.name.first} ${user.name.last}`;
+      username[index].innerHTML = `${user.login.username}`;
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
+// Follow me Button
 btn.forEach((element, index) =>
   element.addEventListener("click", () => {
     console.log(name);
